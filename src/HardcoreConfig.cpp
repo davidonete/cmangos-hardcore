51224774bd085ecdaf01b1825f0401de490443fd
David <davidparraausina@gmail.com>
2024-02-03 20:40:14 +0000

    Add hardcore config files

---
+++ src/HardcoreConfig.cpp
@@ -0,0 +1,42 @@
+#include "AchievementsConfig.h"
+
+#include "Log.h"
+#include "SystemConfig.h"
+
+AchievementsConfig::AchievementsConfig()
+: enabled(false)
+, sendMessage(false)
+, sendAddon(false)
+, sendVisual(false)
+, randomBots(false)
+, randomBotsRealmFirst(false)
+, accountAchievements(false)
+, effectId(0)
+{
+
+}
+
+INSTANTIATE_SINGLETON_1(AchievementsConfig);
+
+bool AchievementsConfig::Initialize()
+{
+    sLog.outString("Initializing Achievements by Tsaah");
+
+    if (!config.SetSource(SYSCONFDIR"achievements.conf"))
+    {
+        sLog.outError("Failed to open configuration file achievements.conf");
+        return false;
+    }
+
+    enabled = config.GetBoolDefault("Achievements.Enable", false);
+    sendMessage = config.GetBoolDefault("Achievements.SendMessage", false);
+    sendAddon = config.GetBoolDefault("Achievements.SendAddon", false);
+    sendVisual = config.GetBoolDefault("Achievements.SendVisual", false);
+    randomBots = config.GetBoolDefault("Achievements.RandomBots", false);
+    randomBotsRealmFirst = config.GetBoolDefault("Achievements.RandomBotsRealmFirst", false);
+    accountAchievements = config.GetBoolDefault("Achievements.AccountAchievenemts", false);
+    effectId = config.GetIntDefault("Achievements.EffectId", 0);
+
+    sLog.outString("Achievements configuration loaded");
+    return true;
+}
